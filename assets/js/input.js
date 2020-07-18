825e0a5c9228af29f68dbd77364c6666cf4e14f4
Joao Brilhante <joao.brilhante@outlook.com>
2020-07-18 05:12:27 +0200

    Update the field style and script.

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,74 +1,108 @@
 (function($) {
-	// Initialize Cookies in no conflict mode.
-	var $acf_intl_tel_input_cookies = Cookies.noConflict();
+
+	// Initialize cookies in no conflict mode.
+	let $acf_intl_tel_input_cookies = Cookies.noConflict();
 
 	/**
 	 *  initialize_field
 	 *
 	 *  This function will initialize the $field.
 	 *
-	 *  @date	30/11/17
-	 *  @since	5.6.5
+	 *  @date   30/11/17
+	 *  @since  5.6.5
 	 *
-	 *  @param	n/a
-	 *  @return	n/a
+	 *  @return n/a
+	 *  @param  $field
 	 */
 
-	function initialize_field($field) {
-		// get input field
-		var $telInput = $field.find('input[type=tel]');
-		// set telInput options
-		var $telInputOptions = {
+	function initialize_field( $field ) {
+
+		// Field.
+		const $phoneInput = $field.find('input[type=tel]');
+
+		// Hidden field.
+		const $hiddenName = $phoneInput.attr('data-hiddenInput');
+		const $hiddenInput = $('input[name=acf\\[' + $hiddenName + '\\]]');
+
+		// Field attributes.
+		const $allowDropdown = $phoneInput.attr('data-allowDropdown');
+		const $excludeCountries = $phoneInput.attr('data-excludeCountries');
+		const $initialCountry = $phoneInput.attr('data-initialCountry');
+		const $onlyCountries = $phoneInput.attr('data-onlyCountries');
+		const $placeholderNumberType = $phoneInput.attr('data-placeholderNumberType');
+		const $preferredCountries = $phoneInput.attr('data-preferredCountries');
+		const $separateDialCode = $phoneInput.attr('data-separateDialCode');
+
+		// Field options.
+		const $phoneOptions = {
+			allowDropdown: ($allowDropdown === '1'),
 			autoPlaceholder: "aggressive",
-			hiddenInput: $telInput.attr('data-hiddenInput'),
-			separateDialCode: ($telInput.attr('data-separateDialCode') == '1'),
-			allowDropdown: ($telInput.attr('data-allowDropdown') == '1'),
-			initialCountry: $telInput.attr('data-initialCountry'),
-			preferredCountries: ($telInput.attr('data-preferredCountries') != '') ? $telInput.attr('data-preferredCountries').split(',') : '',
-			onlyCountries: ($telInput.attr('data-onlyCountries') != '') ? $telInput.attr('data-onlyCountries').split(',') : '',
-			excludeCountries: ($telInput.attr('data-excludeCountries') != '') ? $telInput.attr('data-excludeCountries').split(',') : '',
-			// placeholderNumberType: "MOBILE",
+			excludeCountries: ($excludeCountries !== '') ? $excludeCountries.split(',') : '',
+			initialCountry: $initialCountry,
+			onlyCountries: ($onlyCountries !== '') ? $onlyCountries.split(',') : '',
+			placeholderNumberType: $placeholderNumberType,
+			preferredCountries: ($preferredCountries !== '') ? $preferredCountries.split(',') : '',
+			separateDialCode: ($separateDialCode === '1'),
 		};
-		// If initialCountry is set to Auto, we need to lookup user location
-		if ($telInput.attr('data-initialCountry') == 'auto') {
-			$telInputOptions.geoIpLookup = function(callback) {
-				// Check if we have user location saved in cookies
-				var $countryCodeCookie = $acf_intl_tel_input_cookies.get('acf_intl_tel_input_countryCode');
+
+		// Geographical IP lookup.
+		if ($initialCountry === 'auto') {
+			$phoneOptions.geoIpLookup = function(callback) {
+				// Check if we have user location saved in cookies.
+				const $countryCodeCookie = $acf_intl_tel_input_cookies.get('acf_intl_tel_input_countryCode');
 				if ($countryCodeCookie === undefined) {
-					// If Location is not saved, let's get it!
-					$.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-						var countryCode = (resp && resp.country) ? resp.country : "";
-						// Save location to Cookies
+					// If location is not saved, let's get it!
+					$.get('https://ipinfo.io', function() {}, 'jsonp').always(function(resp) {
+						const countryCode = (resp && resp.country) ? resp.country : '';
+
+						// Save location to cookies.
 						$acf_intl_tel_input_cookies.set('acf_intl_tel_input_countryCode', countryCode, {
 							expires: 7,
-							path: acf_intl_tel_input_obj.COOKIEPATH,
-							domain: acf_intl_tel_input_obj.COOKIE_DOMAIN
+							path: $acf_intl_tel_input_cookies.COOKIEPATH,
+							domain: $acf_intl_tel_input_cookies.COOKIE_DOMAIN
 						});
-						// Execute the callback
+
+						// Execute the callback.
 						callback(countryCode);
 					});
 				} else {
-					// Execute the callback
+					// Execute the callback.
 					callback($countryCodeCookie);
 				}
 			};
 		}
-		// Make input render the mask when user focus out
-		$telInput.on('blur', function() {
-				if ($telInput.intlTelInput("isValidNumber")) {
-					$telInput.intlTelInput("setNumber", $telInput.intlTelInput('getNumber'));
-					$telInput.prev('input[type="hidden"]').val($telInput.intlTelInput('getNumber'));
-				} else { // Clear input if number is not valid
-					$telInput.val('');
-					$telInput.prev('input[type="hidden"]').val('');
-				}
-			})
-			// Initialize Input
-			.intlTelInput($telInputOptions);
+
+		// Field focus out event.
+		$phoneInput.on('blur', function() {
+			// Check if the phone is valid.
+			if ($phoneInput.intlTelInput("isValidNumber")) {
+				// Get phone in both formats.
+				const phone = $phoneInput.intlTelInput('getNumber', intlTelInputUtils.numberFormat.E164);
+				const nationalPhone = $phoneInput.intlTelInput('getNumber', intlTelInputUtils.numberFormat.NATIONAL);
+
+				// Create phone object.
+				const phoneObject = {
+					phone,
+					nationalPhone
+				};
+
+				// Update the fields.
+				$phoneInput.intlTelInput('setNumber', phone);
+				$hiddenInput.val(JSON.stringify(phoneObject));
+			} else {
+				// Clear the fields.
+				$phoneInput.val('');
+				$hiddenInput.val('');
+			}
+		});
+
+		// Initialize the input.
+		$phoneInput.intlTelInput($phoneOptions);
+
 	}
 
 
-	if (typeof acf.add_action !== 'undefined') {
+	if ( typeof acf.add_action !== 'undefined' ) {
 
 		/*
 		 *  ready & append (ACF5)
@@ -85,12 +119,5 @@
 		acf.add_action('append_field/type=intl_tel_input', initialize_field);
 
 	}
-
-	/**
-	 * Apply the initialize_fields in Frontend acf_form
-	 */
-	$('[data-type="intl_tel_input"]').each(function() {
-		initialize_field($(this));
-	});
 
 })(jQuery);
